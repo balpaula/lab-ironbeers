9291e19813b632e10da0c6ccb9d6041e5f49c182
Paula Balcells <paulabalcells@gmail.com>
2018-07-19 10:56:23 +0200

    Iteration 1

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 
 const express = require('express');
 const hbs     = require('hbs');
-const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+const app     = express();
+
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  res.render('beers');
+});
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('random-beers');
 });
 
 
